cb7f175e0be537290f23a381bb37e451872a05b9
gorimatyan <gorima102131@gmail.com>
2025-03-09 11:33:16 +0900

    feat: ズーム機能追加と表示する絵文字とかデータを本番に近くした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import { AppleMap } from "@/components/AppleMap/AppleMap"
 
 export default function Home() {
   return (
-    <div className="p-2 w-full h-[90dvh]">
-      <h1 className="text-2xl font-bold">福岡県</h1>
+    <div className="w-full h-[100dvh]">
       <AppleMap
         centerPoint={[33.5902, 130.4017]}
         zoom={12}
@@ -11,23 +10,23 @@ export default function Home() {
         mapAnnotationData={[
           {
             id: 1,
-            category: "エンタメ",
+            category: "火事",
             location: { lat: 33.5902, lng: 130.4017 },
-            title: "福岡タワー",
-            subtitle: "福岡のランドマーク",
+            title: "フロートで沖に流され転落事故、注意喚起！",
+            subtitle: "福岡県福岡市東区 志賀島",
             clusteringIdentifier: "fukuoka",
             data: {
               area: "福岡市中央区",
-              link: "https://ja.wikipedia.org/wiki/福岡タワー",
+              link: "https:\/\/mainichi.jp",
             },
             // marker: "/images/m3.webp",
           },
           {
             id: 2,
-            category: "エンタメ",
+            category: "火事",
             location: { lat: 33.5952, lng: 130.4027 },
-            title: "大濠公園",
-            subtitle: "福岡のランドマーク",
+            title: "家が燃えました",
+            subtitle: "福岡市中央区",
             clusteringIdentifier: "fukuoka",
             // marker: "/images/m3.webp",
             data: {
@@ -37,10 +36,10 @@ export default function Home() {
           },
           {
             id: 3,
-            category: "交通",
+            category: "殺人",
             location: { lat: 33.5972, lng: 130.4047 },
-            title: "天神駅",
-            subtitle: "福岡のランドマーク",
+            title: "駅付近で殺人事件",
+            subtitle: "福岡市東区",
             clusteringIdentifier: "fukuoka",
             // marker: "/images/m3.webp",
             data: {
@@ -50,10 +49,10 @@ export default function Home() {
           },
           {
             id: 4,
-            category: "交通",
+            category: "殺人",
             location: { lat: 33.6192, lng: 130.4197 },
-            title: "天神駅",
-            subtitle: "福岡のランドマーク",
+            title: "道端で殺人事件",
+            subtitle: "福岡市東区",
             clusteringIdentifier: "fukuoka",
             // marker: "/images/m3.webp",
             data: {
@@ -63,10 +62,10 @@ export default function Home() {
           },
           {
             id: 5,
-            category: "交通",
+            category: "殺人",
             location: { lat: 33.6082, lng: 130.4192 },
-            title: "薬院大通駅",
-            subtitle: "福岡のランドマーク",
+            title: "薬院大通駅で事件発生。",
+            subtitle: "福岡市東区",
             clusteringIdentifier: "fukuoka",
             // marker: "/images/m3.webp",
             data: {
---
+++ src/components/Icons/ChevronIcon.tsx
@@ -0,0 +1,20 @@
+type Props = React.SVGProps<SVGSVGElement>
+
+export const ChevronIcon = (props: Props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18.43"
+      height="24"
+      viewBox="0 0 18.43 24"
+      {...props}
+    >
+      <path
+        id="FontAwsome_chevron-left_"
+        data-name="FontAwsome (chevron-left)"
+        d="M27.981,48.73,41.5,38.049a2,2,0,0,1,2.36,0l1.577,1.246a1.134,1.134,0,0,1,0,1.863l-10.712,8.5,10.711,8.5a1.134,1.134,0,0,1,0,1.863l-1.577,1.246a2,2,0,0,1-2.36,0L27.981,50.595A1.134,1.134,0,0,1,27.981,48.73Z"
+        transform="translate(-27.493 -37.662)"
+      />
+    </svg>
+  )
+}
